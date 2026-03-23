daf0e190eaba0bdd9e29e6588b270fc712dabd50
Dongxu Zhang <2323135182@qq.com>
2026-03-23 23:55:05 +0800

    init

--- openai-code-review-sdk/src/main/java/com/zdx/ai/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/com/zdx/ai/sdk/OpenAiCodeReview.java
@@ -6,7 +6,7 @@ package com.zdx.ai.sdk;
  * @create 2026-03-23 22:19
  */
 public class OpenAiCodeReview {
-    public static void main(String[] args) {
+    public static void main(String[] args) { // 本文件用于gitHub的actions来执行的文件
         System.out.println("测试执行");
     }
 }
