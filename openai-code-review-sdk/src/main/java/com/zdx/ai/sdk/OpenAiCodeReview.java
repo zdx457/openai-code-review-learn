edae138ceb3be0537017d9518136e2c7461e505f
Dongxu Zhang <2323135182@qq.com>
2026-03-24 11:24:58 +0800

    github action test

--- openai-code-review-sdk/src/main/java/com/zdx/ai/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/com/zdx/ai/sdk/OpenAiCodeReview.java
@@ -1,10 +1,5 @@
 package com.zdx.ai.sdk;
 
-/**
- * @author zdx
- * @description
- * @create 2026-03-23 22:19
- */
 public class OpenAiCodeReview {
     public static void main(String[] args) { // 本文件用于gitHub的actions来执行的文件
         System.out.println("测试执行");
