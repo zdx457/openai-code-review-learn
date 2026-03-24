d00586d0445dbff99bfc4de2d49deef000c4b5e3
Dongxu Zhang <2323135182@qq.com>
2026-03-24 12:04:55 +0800

    检出代码

--- openai-code-review-sdk/src/main/java/com/zdx/ai/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/com/zdx/ai/sdk/OpenAiCodeReview.java
@@ -1,7 +1,27 @@
 package com.zdx.ai.sdk;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
 public class OpenAiCodeReview {
-    public static void main(String[] args) { // 本文件用于gitHub的actions来执行的文件
+    public static void main(String[] args) throws Exception { // 本文件用于gitHub的actions来执行的文件
         System.out.println("测试执行");
+
+        // 1.代码检出
+
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1","HEAD");
+        processBuilder.directory(new File("."));
+        Process process = processBuilder.start();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        StringBuilder diffCode = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            diffCode.append(line);
+        }
+
+        int exitCode = process.waitFor();
+        System.out.println("Exit with code: " + exitCode);
+        System.out.println("评审代码"+diffCode.toString());
     }
 }
