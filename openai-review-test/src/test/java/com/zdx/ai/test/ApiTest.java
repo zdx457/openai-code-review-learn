51d4c5c7ca9020b131836bbaa5f9f0786338faae
Dongxu Zhang <2323135182@qq.com>
2026-03-26 10:50:54 +0800

    消息模板通知 测试v6

--- openai-review-test/src/test/java/com/zdx/ai/test/ApiTest.java
+++ openai-review-test/src/test/java/com/zdx/ai/test/ApiTest.java
@@ -18,6 +18,8 @@ public class ApiTest {
         System.out.println("abc1236");
         System.out.println("abc1236");
         System.out.println("abc1236");
+        System.out.println("abc1236");
+        System.out.println("1111111");
 
 
     }
