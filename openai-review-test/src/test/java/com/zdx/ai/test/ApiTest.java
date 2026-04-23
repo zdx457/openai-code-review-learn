0efd55b0d77b8e9912b339d454874fafcc879068
Dongxu Zhang <2323135182@qq.com>
2026-04-23 14:25:36 +0800

    测试

--- openai-review-test/src/test/java/com/zdx/ai/test/ApiTest.java
+++ openai-review-test/src/test/java/com/zdx/ai/test/ApiTest.java
@@ -13,22 +13,6 @@ public class ApiTest {
     @Test
     public void test() {
         System.out.println("aaa1234");
-        System.out.println("aaa1234");
-        System.out.println("aaa1234");
-        System.out.println("aaa1234");
-        System.out.println("aaa1234");
-        System.out.println("aaa1234");
-        System.out.println("aaa1234");
-        System.out.println("aaa1234");
-        System.out.println("aaa1234");
-        System.out.println("aaa1234");
-        System.out.println("aaa1234");
-        System.out.println("aaa1234");
-        System.out.println("aaa1234");
-        System.out.println("aaa1234");
-        System.out.println("aaa1234");
-        System.out.println("aaa1234");
-        System.out.println("aaa1234");
 
 
 
